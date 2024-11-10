9e12ec1d1129e6262dc3024169550e8fe39a5a19
Куликов Валерий Романович <VRKulikov@beeline.ru>
2024-11-10 13:24:57 +0300

    Task-1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Author authorOfBookOnMicroservices = new Author("Chris", "Richardson");
+        Author authorOfBookOnAPIDesignPatterns = new Author("JJ", "Geewax");
+
+        Book bookOnMicroservices = new Book("Microservices", authorOfBookOnMicroservices, 2019);
+        Book bookOnAPIDesignPatterns = new Book("API Design Patterns", authorOfBookOnAPIDesignPatterns, 2020);
+
+        System.out.println(authorOfBookOnMicroservices.getFirstName() + " " + authorOfBookOnMicroservices.getLastName());
+        System.out.println(authorOfBookOnAPIDesignPatterns.getFirstName() + " " + authorOfBookOnAPIDesignPatterns.getLastName());
+
+        System.out.println(bookOnAPIDesignPatterns.getName() + " " + bookOnAPIDesignPatterns.getAuthor().getFirstName() + " " + bookOnAPIDesignPatterns.getAuthor().getLastName() + " " + bookOnAPIDesignPatterns.getYearOfPublication());
+
+        bookOnAPIDesignPatterns.setYearOfPublication(2021);
+
+        System.out.println(bookOnMicroservices.getName() + " " + bookOnMicroservices.getAuthor().getFirstName() + " " + bookOnMicroservices.getAuthor().getLastName() + " " + bookOnMicroservices.getYearOfPublication());
+        System.out.println(bookOnAPIDesignPatterns.getName() + " " + bookOnAPIDesignPatterns.getAuthor().getFirstName() + " " + bookOnAPIDesignPatterns.getAuthor().getLastName() + " " + bookOnAPIDesignPatterns.getYearOfPublication());
     }
 }
